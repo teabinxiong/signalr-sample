fdeaa29e52881f16a57a44d2ddd8866a56a6faae
DOMOCI  | Tea Bin Xiong <65224341+teabinxiong@users.noreply.github.com>
2023-04-24 17:03:44 +0800

    setup signalr project

---
+++ SignalRServer/Hubs/LearningHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServer.Hubs
+{
+    public class LearningHub : Hub<ILearningHubClient>
+    {
+
+        public async Task BroadcastMessage(string message)
+        {
+            await Clients.All.ReceiveMessage(message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
