8a221ece65f13040daaf8fc3d080eaadd0a511b6
DOMOCI  | Tea Bin Xiong <65224341+teabinxiong@users.noreply.github.com>
2023-04-24 19:44:19 +0800

    add console client

---
+++ DotnetClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("Please specify the URL of SignalR Hub");
+
+var url = Console.ReadLine();
+
+var hubConnection = new HubConnectionBuilder()
+                            .WithUrl(url)
+                            .Build();
+
+hubConnection.On<string>("ReceiveMessage", message => Console.WriteLine($"SignalR Hub Message: {message}"));
+
+try
+{
+    await hubConnection.StartAsync();
+
+    while (true)
+    {
+        var message = string.Empty;
+
+        Console.WriteLine("Please specify the action:");
+        Console.WriteLine("0 - broadcast to all");
+        Console.WriteLine("exit - Exit the program");
+
+        var action = Console.ReadLine();
+
+        Console.WriteLine("Please specify the message:");
+        message = Console.ReadLine();
+
+        if (action == "exit")
+            break;
+
+        await hubConnection.SendAsync("BroadcastMessage", message);
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+    return;
+}
