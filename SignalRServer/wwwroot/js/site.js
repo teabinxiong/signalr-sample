8a2412dc6f81c05fb351871e4182f65989ae530f
DOMOCI  | Tea Bin Xiong <65224341+teabinxiong@users.noreply.github.com>
2023-04-24 18:48:46 +0800

    Added javascript client

--- SignalRServer/wwwroot/js/site.js
+++ SignalRServer/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/learningHub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
 
-// Write your JavaScript code.
+connection.on("ReceiveMessage", (message) => {
+    $('#signalr-message-panel').prepend($('<div />').text(message));
+});
+
+$('#btn-broadcast').click(function () {
+    var message = $('#broadcast').val();
+    connection.invoke("BroadcastMessage", message).catch(err => console.error(err.toString()));
+});
+
+async function start() {
+    try {
+        await connection.start();
+        console.log('connected');
+    } catch (err) {
+        console.log(err);
+        setTimeout(() => start(), 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
+
+start();
